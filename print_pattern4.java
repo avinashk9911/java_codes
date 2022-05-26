d8e42ae7c4ee61130b287ca7a602742f44992866
avinashk9911 <90620845+avinashk9911@users.noreply.github.com>
2022-05-26 13:59:44 +0530

    Add files via upload

---
+++ print_pattern4.java
@@ -0,0 +1,16 @@
+//print pattern 5
+//              54
+//              543
+//              5432
+//              54321
+
+public class print_pattern4 {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 5; j >= i; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
