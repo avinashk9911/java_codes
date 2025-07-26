5be69129be8d9e5b141e8cdf3d8e66aff9c6bc53
Avinash <aviku9911@gmail.com>
2025-07-26 23:41:00 +0530

    new java revision file

---
+++ switchCase.java
@@ -0,0 +1,19 @@
+public class switchCase {
+    public static void main(String[] args){
+        int day = 3;
+        switch(day){
+            case 1:
+                System.out.println("Day 1");
+                break;
+            case 2:
+                System.out.println("Day 2");
+                break;
+            case 3:
+                System.out.println("Day 3");
+                break;
+
+            default:
+                System.out.println("wrong day");
+        }
+    }
+}
